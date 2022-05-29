a00aba2d9c8db5c8aca119551ecec685d032fc27
Astahta <fiqi9e@gmail.com>
2022-05-29 13:55:16 +0700

    create constant of color palette and endpoint

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+//Define color palette for apps
+class BasePalette {
+  static Color success    = Color(0xff0b945e);
+  static Color danger         = Color(0xffd4423a);
+  static Color bg           = Color(0xFFFFFFFF);
+  static Color primary      = Color(0xFFFFFFFF);
+  static Color accent       = Color(0xff68a9e3);
+  static Color accentMedium = Color(0xFF866E48);
+  static Color black  = Color(0xFF010101);
+}
+
+// Define endpoint of Itunes API
+class BaseString{
+  static String baseUrl    = "https://itunes.apple.com/";
+}
